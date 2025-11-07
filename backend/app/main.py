4eacec1ed993c8273d051c4317303e798ad17153
parthu1029 <parthasaradhiganta@gmail.com>
2025-11-07 12:15:47 +0530

    added frontend cors

--- backend/app/main.py
+++ backend/app/main.py
@@ -32,6 +32,9 @@ app = FastAPI(
 if settings.BACKEND_CORS_ORIGINS:
     app.add_middleware(
         CORSMiddleware,
+        allow_origins=[str(origin) for origin in settings.BACKEND_CORS_ORIGINS] + [
+            "https://*.vercel.app",
+        ],
         allow_origins=[str(origin) for origin in settings.BACKEND_CORS_ORIGINS],
         allow_origin_regex=r"^https?://(localhost|127\.0\.0\.1)(:\d+)?$",
         allow_credentials=True,
